6ccf37f80026aabfb6fe766eb69201c793e064b8
Matt Carpenter <mattcarpenter@gmail.com>
2020-04-21 22:41:01 +0900

    small bugfix

--- webapp/src/components/PerformanceReviewForm/submit.js
+++ webapp/src/components/PerformanceReviewForm/submit.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { CREATE_PERFORMANCE_REVIEW_MODAL } from '../../constants/Modals';
 import { hideModal } from '../../stores/modals/ModalsActions';
+import { getEmployee } from '../../stores/employees/EmployeesActions';
 
 export function submitCreate(values, dispatch) {
   const payload = {
@@ -11,6 +12,7 @@ export function submitCreate(values, dispatch) {
   return axios
       .post('/api/v1/performance-reviews/', payload)
       .then(({data}) => {
+        dispatch(getEmployee(values.employeeId));
         dispatch(hideModal(CREATE_PERFORMANCE_REVIEW_MODAL));
       }).catch(({response : { data }}) => {
         // not yet implemented
